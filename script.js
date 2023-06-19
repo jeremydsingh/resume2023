84a56bad73d3bcfce1e11b751d4391b13f926d9c
Jeremy Singh <jeremy.singh@gmail.com>
2023-06-19 18:54:45 +0200

    Media queries and finished js.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //toggle icon navbar
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 //scroll sections
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
@@ -27,4 +35,8 @@ window.onscroll= () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky',window.scrollY > 100);
+
+    // remove toggle icon and navbar when click links
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
